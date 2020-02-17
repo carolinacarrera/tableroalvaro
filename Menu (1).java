0c9e6b12d5e61d9375925aaa495150be3b176b41
carolinacarrera <60191092+carolinacarrera@users.noreply.github.com>
2020-02-17 09:51:03 -0300

    Add files via upload

---
+++ Menu (1).java
@@ -0,0 +1,22 @@
+package oscurilandia;
+
+import javax.swing.JOptionPane;
+
+public class Menu {
+	private static int accion;
+	Tablero tablero1= new Tablero();
+
+	public static void main(String[] args) {
+		
+		
+		
+		do {
+		int accion = Integer.parseInt(JOptionPane.showInputDialog("Presione 1 para disparar|2 para abandonar"));
+		tablero1.
+		}while(accion!=2);
+		
+		
+
+	}
+
+}
